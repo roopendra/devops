db4b0c63aacff8a8aa220868f73298c50fd9c74f
Roopendra Vishwakarma <roopendramca@gmail.com>
2023-04-19 11:06:32 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-064087b8d355e9051"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
